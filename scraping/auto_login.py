2a6d9e48aca41b8abd4ef23c90bbbbbbe166d0e1
kazaneza <kazanezagentil@gmail.com>
2024-05-06 11:34:35 +0200

    auto login and click

--- scraping/auto_login.py
+++ scraping/auto_login.py
@@ -20,6 +20,8 @@ def main():
     driver.find_element(by="id", value="id_username").send_keys("automated")
     time.sleep(2)
     driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+    
+    time.sleep(2)
+    driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
     print(driver.current_url)
-
 print(main())
